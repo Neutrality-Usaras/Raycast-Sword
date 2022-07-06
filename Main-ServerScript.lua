ef60e8bd110d7aa277322035ac655f89437d61b5
Neutrality-Usaras <57842146+Neutrality-Usaras@users.noreply.github.com>
2022-07-06 19:35:45 +0600

    Update Main-ServerScript.lua

--- Main-ServerScript.lua
+++ Main-ServerScript.lua
@@ -5,43 +5,18 @@ local repStorage = game:GetService('ReplicatedStorage')
 
 local modelsFolder = repStorage:WaitForChild("Models") 
 
---Events
-local eventsFolder = repStorage.Events
-
-local ShiftEquip = eventsFolder:WaitForChild("ShiftEquip") --Shift Equip Murasama
-local BackToScadbard = eventsFolder:WaitForChild("BackToScadbard")
-local WeldToHand = eventsFolder:WaitForChild("WeldToHand")
-local WeldToLeft = eventsFolder:WaitForChild("WeldToLeft")
-local WeldToRight = eventsFolder:WaitForChild("WeldToRight")
-
+--//Events
+local eventsFolder = repStorage:WaitForChild('Events')
+local mainRemote = eventsFolder:WaitForChild('remote')
 local MurasamaModule = require(serverStorage.Murasama) --Murasama Module
 
 players.PlayerAdded:Connect(function(plr)
-	local Chrctr = plr.CharacterAdded:Wait()
-	local Torso = plr.Character.UpperTorso
-	MurasamaModule:M6DToTorso(plr, Torso, modelsFolder)
+	MurasamaModule.M6DToTorso({['plr'] = plr,['modelsFolder'] = modelsFolder})
 end)
 
-players.PlayerRemoving:Connect(function(plr)
-	MurasamaModule:PlayerDeleter(plr)
-end)
+players.PlayerRemoving:Connect(MurasamaModule.PlayerDeleter)
 
-ShiftEquip.OnServerEvent:Connect(function(Plr)
-	MurasamaModule:ShiftEquip(Plr)
-end)
 
-BackToScadbard.OnServerEvent:Connect(function(Plr)
-	MurasamaModule:BackToScabbard(Plr)
-end)
-
-WeldToHand.OnServerEvent:Connect(function(Plr)
-	MurasamaModule:WeldToArm(Plr)
-end)
-
-WeldToLeft.OnServerEvent:Connect(function(Plr)
-	MurasamaModule:WeldToLeft(Plr)
-end)
-
-WeldToRight.OnServerEvent:Connect(function(Plr)
-	MurasamaModule:WeldToRight(Plr)
+mainRemote.OnServerEvent:Connect(function(plr, actionName, ...)
+	MurasamaModule[actionName](plr, ...)
 end)
