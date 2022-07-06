62485d326d426e1c6ab1a54bf1efbe6859f283a5
Neutrality-Usaras <57842146+Neutrality-Usaras@users.noreply.github.com>
2022-07-06 19:36:33 +0600

    Update Main-Sword Module.lua

--- Main-Sword Module.lua
+++ Main-Sword Module.lua
@@ -10,13 +10,16 @@ MurasamaModule.__index = MurasamaModule
 
 local playersMurasamas = {} 
 
+local examplesFolder = workspace.Examples
 local exampleModels = {
-	["scadbardMotor"] = workspace.RealTimePrototype.UpperTorso.SwordTorso.ScadbardMotor,
-	["rightHandMotor"] = workspace.ANIMATE3.RightHand.BladeMotor,
-	['leftHandMotor'] = workspace.ANIMATE3.LeftHand.ScadbardMotor,
+	["scadbardMotor"] = examplesFolder.RealTimePrototype.UpperTorso.SwordTorso.ScadbardMotor,
+	["rightHandMotor"] = examplesFolder.ANIMATE3.RightHand.BladeMotor,
+	['leftHandMotor'] = examplesFolder.ANIMATE3.LeftHand.ScadbardMotor,
 	['bladeMotor'] = workspace.AllMurasamasVersions.ModernTorso.Sword.Blade.BladeMotor.bnsW,
-	['leftBladeMotor'] = workspace.Animatti.LeftHand.BladeMotor
+	['leftBladeMotor'] = examplesFolder.Animatti.LeftHand.BladeMotor
 } 
+
+local plrCD = {}
 
 setmetatable(MurasamaModule, playersMurasamas)
 setmetatable(MurasamaModule, exampleModels)
@@ -27,6 +30,9 @@ local rightHandMotor = exampleModels.rightHandMotor
 local leftHandMotor = exampleModels.leftHandMotor
 local bladeMotor = exampleModels.bladeMotor
 local leftBladeMotor = exampleModels.leftBladeMotor
+
+--//Services
+local Debris = game:GetService('Debris')
 
 --Other vars
 local waitingTime = 1.2
@@ -46,24 +52,41 @@ local function WeldCreator(...)
 	return Weld
 end
 
-function MurasamaModule:PlayerDeleter(plr)
-	playersMurasamas[plr] = nil
+local function CreateVelocity(...)
+	local chrtct, acceleration, extraPower = unpack(...)
+	local HRP = chrtct.HumanoidRootPart
+	
+	local BodyVelocity = Instance.new("BodyVelocity", HRP)
+	BodyVelocity.MaxForce = Vector3.new(math.huge,math.huge,math.huge)
+	HRP.CFrame = CFrame.new(HRP.Position, extraPower.Position)
+	BodyVelocity.Velocity = -(extraPower.CFrame.lookVector * acceleration)
+	
+
+	return BodyVelocity
 end
 
+function MurasamaModule.PlayerDeleter(plr)
+	playersMurasamas[plr] = nil
+	plrCD[plr] = nil
+end
 
-
-function MurasamaModule:M6DToTorso(plr, torso, modelsFolder) --MOTOR 6D to TORSO
-	local murasama = modelsFolder.SwordTorso:Clone()--MURASAMA
+function MurasamaModule:M6DToTorso() --MOTOR 6D to TORSO
+	
+	local Chrctr = self.plr.CharacterAdded:Wait()
+	local torso = self.plr.Character.UpperTorso
+	
+	local murasama = self.modelsFolder.SwordTorso:Clone()--MURASAMA
 	murasama.Parent = torso
 	
 	local M6D = Instance.new("Motor6D", torso)
 	M6D.Part0 = torso
 	M6D.Part1 = murasama
 	
-	playersMurasamas[plr] = murasama.Sword	
+	playersMurasamas[self.plr] = murasama.Sword
+	plrCD[self.plr] = false
 end
 
-function MurasamaModule:WeldToArm(plr)
+function MurasamaModule.WeldToArm(plr)
 	--Murasama Connected vars
 	local playerMurasama = playersMurasamas[plr]
 	local swordTorso = playersMurasamas[plr].Parent
@@ -82,7 +105,20 @@ function MurasamaModule:WeldToArm(plr)
 	local RHandW = WeldCreator({rightHand, 'Weld', rightHand, blade, rightHandMotor.C0, rightHandMotor.C1})
 end
 
-function MurasamaModule:ShiftEquip(plr) --SHIFT EQUIP
+
+function MurasamaModule.ArmWeld(plr, ...)
+	local armToConnect, armToDelete, Motor = unpack(...)
+	
+	local playerMurasama = playersMurasamas[plr]
+	local Blade = playerMurasama.Blade.BladeMotor
+	
+	armToDelete.Weld:Destroy()
+
+	WeldCreator({armToConnect, 'Weld', armToConnect, Blade, exampleModels[Motor].C0, exampleModels[Motor].C1})
+end
+
+function MurasamaModule.ShiftEquip(plr) --SHIFT EQUIP
+	
 	local swordTorso = playersMurasamas[plr].Parent
 	local playerMurasama = playersMurasamas[plr]
 	local scabbard = playersMurasamas[plr].Scabbard.ScadbardMotor
@@ -111,8 +147,7 @@ function MurasamaModule:ShiftEquip(plr) --SHIFT EQUIP
 	end
 end
 
-function MurasamaModule:BackToScabbard(plr)
-	
+function MurasamaModule.BackToScabbard(plr)
 	local swordTorso = playersMurasamas[plr].Parent
 	local playerMurasama = playersMurasamas[plr]
 	local scabbard = playerMurasama.Scabbard.ScadbardMotor
@@ -127,31 +162,55 @@ function MurasamaModule:BackToScabbard(plr)
 	local WeldToScad = WeldCreator({blade, 'bnsW', blade, scabbard, bladeMotor.C0, bladeMotor.C1})
 end
 
-function MurasamaModule:WeldToLeft(plr)
+
+local raycastParams = RaycastParams.new()
+
+function MurasamaModule.CreateRay(plr)
 	local playerMurasama = playersMurasamas[plr]
+	local rayPart = plr.Character.HumanoidRootPart
 	
-	local LeftH = plr.Character:WaitForChild("LeftHand")
-	local RightH = plr.Character:WaitForChild("RightHand")
+	local rayDirection = rayPart.CFrame.lookVector * 10
+	local rayOrigin = rayPart.Position
+	raycastParams.FilterDescendantsInstances = {plr.Character, workspace.Baseplate}
+	raycastParams.FilterType = Enum.RaycastFilterType.Blacklist
 	
-	local Blade = playerMurasama.Blade.BladeMotor
+	local raycastResult = workspace:Raycast(rayOrigin, rayDirection, raycastParams)
 	
-	RightH.Weld:Destroy()
+	--local rayPart2 = playerMurasama.Blade.rayPart
+	--local rayOrig2 = rayPart2.Position
+	--local rayDir2 = rayPart2.CFrame.lookVector * 100
 	
-	local LHandW = WeldCreator({LeftH, 'Weld', LeftH, Blade, leftBladeMotor.C0, leftBladeMotor.C1})
+	--local rayResult2 = workspace:Raycast(rayOrig2, rayDir2, raycastParams)
+	
+	if raycastResult then
+		
+		local randomPart = raycastResult.Instance
+		local acceleration = -25.5
+		local extraPower = rayPart.CFrame.lookVector
+		
+		if randomPart.Parent:FindFirstChild('Humanoid') then
+			local preyHumanoid = randomPart.Parent.Humanoid
+			local character = randomPart.Parent
+			 
+			local bodyVelocity = CreateVelocity({character, acceleration, rayPart})
+			preyHumanoid:TakeDamage(7.5)
+			Debris:AddItem(bodyVelocity, .2)
+		end
+	--elseif rayResult2 then
+	--	print(rayResult2.Instance)
+	--	print('Hit by second ray')
+	end
 end
 
-function MurasamaModule:WeldToRight(plr)
-	local playerMurasama = playersMurasamas[plr]
-	
-	local LeftH = plr.Character:WaitForChild("LeftHand")
-	local RightH = plr.Character:WaitForChild("RightHand")
-	
-	local Blade = playerMurasama.Blade.BladeMotor
-	
-	LeftH.Weld:Destroy()
-	
-	local RHandW = WeldCreator({RightH, 'Weld', RightH, Blade, rightHandMotor.C0, rightHandMotor.C1})
+function MurasamaModule.CreateHitbox(plr, ...)
+	if not plrCD[plr] then
+		plrCD[plr] = true
+		local part, damage = unpack(...)
+		local preyHum = part.Parent.Humanoid
+		preyHum:TakeDamage(damage)		
+		wait(4)
+		plrCD[plr] = false
+	end 
 end
-
 
 return MurasamaModule
